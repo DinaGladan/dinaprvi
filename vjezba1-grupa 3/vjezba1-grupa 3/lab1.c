173a95a56e88118fee38e6cc697300a11ca0edd5
DinaGladan <dglada00@fesb.hr>
2023-10-11 21:32:46 +0200

    1.vjezbe

--- vjezba1-grupa 3/vjezba1-grupa 3/lab1.c
+++ vjezba1-grupa 3/vjezba1-grupa 3/lab1.c
@@ -1,6 +1,35 @@
-#include<stdio.h>
+﻿#include<stdio.h>
+#include<stdlib.h>
+#define MAX_LINE 1024
+#define MAX_SIZE 50
+#define FILE_ERROR_OPEN -1
+
+//Napisati program koji prvo pročita koliko redaka ima datoteka, tj.koliko ima studenata
+//zapisanih u datoteci.Nakon toga potrebno je dinamički alocirati prostor za niz struktura
+//studenata(ime, prezime, bodovi) i učitati iz datoteke sve zapise.Na ekran ispisati ime,
+//prezime, apsolutni i relativni broj bodova.
+//Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+//	relatvan_br_bodova = br_bodova / max_br_bodova * 100
+
+typedef struct _student { //tip strukture je _student, a ime je Student
+
+	char name;
+	char surname;
+	double points;
+}Student;
+
+int readNoRowsInFile() {
+	int counter = 0;
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = {0};
+	filePointer= fopen("students.txt", "r");
+
+	fclose(filePointer);
+}
 
 int main() {
-	printf("Hello World");
+	int noRows=0;
+	printf("%d", noRows);
+
 	return 0;
 }

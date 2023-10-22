a87b21ddb2ee5bcf7f0db1295fdc996f5a1d5d02
DinaGladan <dglada00@fesb.hr>
2023-10-23 01:04:49 +0200

    1.task

--- vjezba1-grupa 3/vjezba1-grupa 3/lab1.c
+++ vjezba1-grupa 3/vjezba1-grupa 3/lab1.c
@@ -1,6 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define MAX_LINE 1024
 #define MAX_SIZE 50
 #define FILE_ERROR_OPEN -1
@@ -20,27 +21,100 @@ typedef struct _student { //tip strukture je _student, a ime je Student
 
 }Student;
 
-int readNoRowsInFile() {
-	int counter = 0;
-	FILE* fp = NULL;
+
+int readNoRowsInFile(){
+
+	int counter=0;
 	char buffer[MAX_LINE] = { 0 };
-	fp = fopen("students.txt", "r");
-	if (!fp) {
+
+	FILE* filePointer = NULL;
+	filePointer = fopen("students.txt", "r");
+	
+	if (!filePointer) {
 		printf("\nNema nis");
 		return FILE_ERROR_OPEN;
 	}
-	while (!feof(fp)) {
-		fgets(buffer, MAX_LINE, fp);
+	while (!feof(filePointer)) {
+		fgets(buffer, MAX_LINE, filePointer); //cita liniju pa poveca counter(broj linija) sve dok ne dodje do kraja fajla
 		counter++;
 	}
-
-	fclose(fp);
+	fclose(filePointer);
 	return counter;
 }
 
+//_________________ILI_________________________
+
+/* int readNoRowsInFile() {
+
+	int counter = 0;
+	char buffer[MAX_LINE] = {0};
+
+	FILE* filePointer=fopen("students.txt","r");
+
+	if (filePointer == NULL) {
+		printf("\nNije moguce otvorit datoteku");
+		return FILE_ERROR_OPEN;
+	}
+	if(filePointer != NULL){
+		while (!feof(filePointer)) {
+			fgets(buffer, MAX_LINE, filePointer); //cita liniju pa poveca counter(broj linija) sve dok ne dodje do kraja fajla
+			counter++;
+		}
+	}
+	fclose(filePointer);
+	return counter;
+}
+*/
+
+void readFromFile(Student* students,int noRows) {
+
+
+	FILE* filePointer = fopen("students.txt", "r");
+
+	if (filePointer == NULL) {
+		printf("\nNije moguce otvorit datoteku");
+		return FILE_ERROR_OPEN;
+	}
+
+	if (filePointer != NULL) {
+		for (int i = 0; i < noRows; i++) {
+			fscanf(filePointer, "%s %s %lf", students[i].name, students[i].surname, &students[i].points);
+		}
+	}
+	fclose(filePointer);
+
+}
+
+int RelativeNumberOfPoints(int max_number_of_points, double points) {
+	int rpoints;
+	
+	rpoints = points / max_number_of_points * 100;
+
+return rpoints;
+}
+
 int main() {
+
 	int noRows = 0;
+	int i;
+	int max_number_of_points = 50;
+	double relative_points;
 	noRows = readNoRowsInFile();
-	printf("%d", noRows);
+	printf("\nBroj redova je %d\n", noRows);
+
+	Student* students = (Student*)malloc(noRows * sizeof(Student)); //dinamicka alokacija memorije
+	readFromFile(students, noRows);
+
+	for (i = 0; i < noRows; i++) {
+
+		relative_points = RelativeNumberOfPoints(max_number_of_points, students[i].points);
+		printf("Ime: %s, Prezime: %s, Apsolutni broj bodova: %lf, Relativan broj bodova %lf\n", students[i].name, students[i].surname, students[i].points, relative_points);
+	}
+
 	return 0;
 }
+
+
+
+
+
